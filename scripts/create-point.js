f0a3b5cf0ba68096d1d5daaeafe9985c3989fdb9
D <kr4usdc@gmail.com>
2020-06-03 18:53:57 -0300

    staging half of the class

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -24,6 +24,9 @@ function getCities(event){
     const ufValue = event.target.value
     const url =  `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/microrregioes`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then(res=>res.json())
     .then(cities => {
@@ -39,3 +42,47 @@ function getCities(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change",getCities)
+
+    //itens de coleta
+    //pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+    function handleSelectedItem(event) {
+        const itemLi = event.target
+
+        //adicionar ou remover uma classe com js
+        itemLi.classList.toggle("selected")
+
+        const itemId = itemLi.dataset.id
+
+        //verificar se existem itens selecionados, se sim
+        //pegar os itens selecionados
+
+        const alreadySelected = selectedItems.findIndex(function(item) {
+            const itemFound = item == itemId
+            return itemFound
+        })
+
+        //se já estiverem selecionado, tirar da selecao
+        if (alreadySelected >= 0) {
+            //tirar seleção
+            const filteredItems = selectedItems.filter( item => {
+                const itemIsDifferent = item != itemId
+                return itemIsDifferent
+            })
+            
+            selectedItems = filteredItems
+        } else {
+        //se nao, estiver selecionado, adicionar à seleção
+            selectedItems.push(itemId)
+        }
+        //atualizar o campo escondido com os itens selecionados
+        collectedItems.value = selectedItems
+    }
