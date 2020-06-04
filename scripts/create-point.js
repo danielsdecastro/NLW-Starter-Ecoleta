1f382a2cde9e975d2d8bad4fbd96b5215234d63e
d <kr4usdc@gmail.com>
2020-06-04 16:53:16 -0300

    end of day 3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -2,7 +2,7 @@ function populateUFs(){
     const ufSelect =  document.querySelector("select[name=uf]")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then(res=>res.json())
+    .then(res => res.json())
     .then(states => {
         
         for(const state of states){
@@ -18,8 +18,8 @@ function getCities(event){
     const citySelect =  document.querySelector("[name=city]")
     const stateInput =  document.querySelector("[name=state]")
 
-    const indexOfSelectState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSelectState].text
+    //const indexOfSelectState = event.target.selectedIndex
+    stateInput.value = event.target.options[event.target.selectedIndex].text
 
     const ufValue = event.target.value
     const url =  `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/microrregioes`
@@ -32,7 +32,7 @@ function getCities(event){
     .then(cities => {
         
         for(const city of cities){
-            citySelect.innerHTML +=  `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML +=  `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -43,6 +43,8 @@ document
     .querySelector("select[name=uf]")
     .addEventListener("change",getCities)
 
+/*-------------------------------------------------*/
+    //PARTE DE SELEÇÃO DOS ITENS
     //itens de coleta
     //pegar todos os li's
 const itemsToCollect = document.querySelectorAll(".items-grid li")
